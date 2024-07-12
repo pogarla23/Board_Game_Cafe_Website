a9df0a7a3e51c35cb8420e8171dd9ca3020b633c
sudeysi ansha <sudeysi.ansha@gmail.com>
2024-07-12 07:56:04 -0500

    nahili I have added a landing page

---
+++ src/scripts/landing_script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('my-button');
+
+    button.addEventListener('click', () => {
+        window.location.href = 'https://www.exploretock.com/sipnplay';
+    });
+});
